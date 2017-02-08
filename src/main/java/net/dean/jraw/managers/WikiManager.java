03e3009410d2b0dab3eba04c4720e9d319c736c3
Garret Yoder <garret@polaric.org>
2017-02-07 23:07:50 -0500

    Update WikiManager.java

--- src/main/java/net/dean/jraw/managers/WikiManager.java
+++ src/main/java/net/dean/jraw/managers/WikiManager.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class is responsible for managing a wiki
